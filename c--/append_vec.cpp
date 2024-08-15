60f3ba526784f58fac3fb2e23f76f1b6c226a7ab
Alex Pyattaev <me@example.com>
2024-08-15 11:26:36 +0300

    appending vec in c++

---
+++ c--/append_vec.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+void append_vector(vector<int>& append_to, const vector<int>& append_from)
+{
+	for (auto i: append_from)
+	{
+		append_to.push_back(i);
+	}
+}
+
+int main(void){
+	cout<<"Correct"<<endl;
+	{
+		auto a = vector {1,2,3};
+		auto b = vector {1,2,3};
+		append_vector(a,b);
+		for (auto i: a){
+			cout<<i<<endl;
+		}
+	}
+	cout<<"Correct?"<<endl;
+	{
+		auto a = vector {1,2,3};
+		append_vector(a,a);
+		for (auto i: a){
+			cout<<i<<endl;
+		}
+	}
+}
